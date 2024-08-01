7aa79ba9ad504da7cdb66212293cc397bca5e248
JehanneDussert <jehanne-dussert@hotmail.fr>
2024-08-01 14:00:42 +0200

    rebase

--- Makefile
+++ Makefile
@@ -24,8 +24,7 @@ format:
 
 clean:
 	docker-compose down -v
-	rm -rf api/.pytest_cache
-	rm -rf api/pytest-rest-api/.pytest_cache
+	find . -type f -name '.pytest_cache' -delete
 	find . -type f -name '*.pyc' -delete
 	find . -type d -name '__pycache__' -delete
 	rm -rf web/node_modules
---
+++ api/app/db/configurations.py
@@ -0,0 +1,15 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+
+client = MongoClient(uri, server_api=ServerApi("1"))
+
+db = client.products_db
+collection = db["products"]
